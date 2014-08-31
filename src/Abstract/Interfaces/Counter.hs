a929a1a89bcd44a7935f86a3a94cf8dc8ca494f6
Andrew Darqui <andrew.darqui@gmail.com>
2014-08-30 22:43:31 -0400

    need these helpers

--- src/Abstract/Interfaces/Counter.hs
+++ src/Abstract/Interfaces/Counter.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE DeriveDataTypeable, RecordWildCards #-}
 module Abstract.Interfaces.Counter (
  Counter (..),
- CounterError (..) {-,
+ CounterError (..),
  incr,
  incrBy,
  decr,
@@ -9,7 +9,6 @@ module Abstract.Interfaces.Counter (
  get,
  reset,
  gentleReset
--}
 ) where
 
 
@@ -35,31 +34,29 @@ data Counter m t = Counter {
  _gentleReset :: m ()
 }
 
-{-
-incr :: (Monad m) => Counter m a t -> m t
-incr Counter {..} = _incr _c
+incr :: (Monad m) => Counter m t -> m t
+incr Counter {..} = _incr 
 
 
-incrBy :: (Monad m) => Counter m a t -> t -> m t
-incrBy Counter {..} n = _incrBy _c n
+incrBy :: (Monad m) => Counter m t -> t -> m t
+incrBy Counter {..} n = _incrBy n
 
 
-decr :: (Monad m) => Counter m a t -> m t
-decr Counter {..} = _decr _c
+decr :: (Monad m) => Counter m t -> m t
+decr Counter {..} = _decr
 
 
-decrBy :: (Monad m) => Counter m a t -> t -> m t
-decrBy Counter {..} n = _decrBy _c n
+decrBy :: (Monad m) => Counter m t -> t -> m t
+decrBy Counter {..} n = _decrBy n
 
 
-get :: (Monad m) => Counter m a t -> m (Maybe t)
-get Counter {..} = _get _c
+get :: (Monad m) => Counter m t -> m (Maybe t)
+get Counter {..} = _get
 
 
-reset :: (Monad m) => Counter m a t -> m ()
-reset Counter {..} = _reset _c
+reset :: (Monad m) => Counter m t -> m ()
+reset Counter {..} = _reset
 
 
-gentleReset :: (Monad m) => Counter m a t -> m ()
-gentleReset Counter {..} = _gentleReset _c
--}
+gentleReset :: (Monad m) => Counter m t -> m ()
+gentleReset Counter {..} = _gentleReset
